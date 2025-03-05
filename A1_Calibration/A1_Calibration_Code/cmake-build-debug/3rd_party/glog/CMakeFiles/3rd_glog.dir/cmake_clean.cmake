a719dfb34edfe245f7d510ce94455b0c40318da2
Jackson513ye <jackson513ye@qq.com>
2025-03-05 15:27:44 +0100

    corner.obj

---
+++ A1_Calibration/A1_Calibration_Code/cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../lib/lib3rd_glog.a"
+  "../../lib/lib3rd_glog.pdb"
+  "CMakeFiles/3rd_glog.dir/logging.cc.o"
+  "CMakeFiles/3rd_glog.dir/logging.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3rd_glog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ A1_Calibration/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../bin/Calibration"
+  "../bin/Calibration.pdb"
+  "CMakeFiles/Calibration.dir/calibration.cpp.o"
+  "CMakeFiles/Calibration.dir/calibration.cpp.o.d"
+  "CMakeFiles/Calibration.dir/calibration_method.cpp.o"
+  "CMakeFiles/Calibration.dir/calibration_method.cpp.o.d"
+  "CMakeFiles/Calibration.dir/main.cpp.o"
+  "CMakeFiles/Calibration.dir/main.cpp.o.d"
+  "CMakeFiles/Calibration.dir/matrix_algo.cpp.o"
+  "CMakeFiles/Calibration.dir/matrix_algo.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
